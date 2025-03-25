0a289636cf44edc1a1936f6b377880de75b7d36b
Aryan <aryan2@shorthills.ai>
2025-03-25 11:17:54 +0530

    test main.yml

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,6 +17,6 @@ public class DemoApplication {
 	@GetMapping("/")
 	public String getName()
 	{
-		return "Hello, This is a demo spring boot APP,Hi from office";
+		return "Hello, This is a demo spring boot APP, test 1";
 	}
 }
