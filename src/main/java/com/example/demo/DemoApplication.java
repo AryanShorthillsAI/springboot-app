4e4d9bc31d818d73254b43ff1500d5a5cf79f1fe
Aryan <aryan2@shorthills.ai>
2025-03-25 12:50:22 +0530

    test 5

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,6 +17,6 @@ public class DemoApplication {
 	@GetMapping("/")
 	public String getName()
 	{
-		return "Hello, This is a demo spring boot APP, test 4";
+		return "Hello, This is a demo spring boot APP, test 5";
 	}
 }
